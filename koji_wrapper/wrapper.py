79dfef3440bfa0aec7b3b5a1e9e9eaee669506e3
jguiditta <jguiditt@redhat.com>
2018-03-29 09:43:26 -0400

    Add file_types and supporting methods. (#6)
    This method returns a list of type of files in the archives related to a
    build.  The archives and build can also be queried directly if needed.
    
    Signed-off-by: Jason Guiditta <jason.guiditta@gmail.com>

--- koji_wrapper/wrapper.py
+++ koji_wrapper/wrapper.py
@@ -9,5 +9,42 @@ from koji_wrapper.base import KojiWrapperBase
 
 class KojiWrapper(KojiWrapperBase):
 
-    def placeholder_method():
-        return True
+    def build(self, nvr):
+        """
+        :param nvr: of the desired build
+        :returns: build object from koji
+        """
+        return self.session.getBuild(nvr)
+
+    def archives(self, **kwargs):
+        """
+        :param **kwargs: Any valid named parameter accepted by the koji client's
+            listArchives method
+        :returns: list of archives from koji
+        """
+        return self.session.listArchives(**kwargs)
+
+    def file_types(self, nvr, types=['image']):
+        """
+        :param nvr: of the desired build:
+        :param types: list of koji archive types.  This is currently any of:
+            'maven', 'win', or 'image'
+        :returns: list of file types of given build
+        """
+        build = self.build(nvr)
+        if not build:
+            return None
+
+        file_types = set([])
+        for this_type in types:
+            archives_list = self.archives(buildID=build['id'], type=this_type)
+            if archives_list:
+                for archive in archives_list:
+                    file_types.add(archive['type_name'])
+
+        if len(file_types):
+            return list(file_types)
+
+        # Default
+        #TODO: make sure we actually need this default - can't see why we do.
+        return ['rpm']
--- tests/unit/test_koji_wrapper.py
+++ tests/unit/test_koji_wrapper.py
@@ -10,6 +10,10 @@ from unittest.mock import MagicMock
 sample_url = 'http://kojihub.com'
 sample_topurl = 'http://somerepo.org'
 
+@pytest.fixture()
+def a_koji_wrapper():
+    return build_wrapper(None)
+
 def build_wrapper(this_session):
     return KojiWrapper( url = sample_url,
                         topurl = sample_topurl,
@@ -47,4 +51,42 @@ def test_init_with_koji_wrapper():
     test_wrapper = build_wrapper(my_koji_wrapper)
     assert isinstance(test_wrapper.session,koji.ClientSession)
     assert test_wrapper.session is my_koji_wrapper.session
+
+def test_gets_build(a_koji_wrapper, sample_build):
+    """
+    GIVEN we have a valid KojiWrapper with a session,
+    WHEN we call the build method with a valid nvr,
+    THEN we get a build object back from the koji api
+    """
+    a_koji_wrapper.session.getBuild = MagicMock(return_value=sample_build)
+    b = a_koji_wrapper.build('some_nvr')
+    assert a_koji_wrapper.session.getBuild.called
+    assert isinstance(b,dict)
+    assert 'id' in b
+
+def test_gets_archives(a_koji_wrapper, sample_archives):
+    """
+    GIVEN we have a valid KojiWrapper with a session,
+    WHEN we call the archives method with a valid build id and type,
+    THEN we get a list of achives back from the koji api
+    """
+    a_koji_wrapper.session.listArchives = MagicMock(return_value=sample_archives)
+    arc = a_koji_wrapper.archives(buildID='12345', type='image')
+    assert a_koji_wrapper.session.listArchives.called
+    assert isinstance(arc,list)
+
+def test_returns_file_types(a_koji_wrapper, sample_archives):
+    """
+    GIVEN we have a valid KojiWrapper with a session,
+    WHEN we call the archives method with a valid build id and type,
+    THEN we get a list of file_types in the archive for the given nvr
+    """
+    a_koji_wrapper.session.listArchives = \
+        MagicMock(return_value=sample_archives)
+    a_koji_wrapper.session.getBuild = MagicMock(return_value={'id':'12345'})
+    ft = a_koji_wrapper.file_types('myproject-9.0-20190326.1.el7ost')
+    assert a_koji_wrapper.session.getBuild.called
+    assert a_koji_wrapper.session.listArchives.called
+    assert isinstance(ft,list)
+    assert 'image' in ft
 
