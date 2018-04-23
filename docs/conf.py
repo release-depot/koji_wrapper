36544b60a5a56032817bebbc4b2ab2475a359b24
Jason Guiditta <jason.guiditta@gmail.com>
2018-04-23 11:48:26 -0400

    One more try to get docs working with rpm mocked out.
    Signed-off-by: Jason Guiditta <jason.guiditta@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ class Mock(MagicMock):
         def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['koji']
+MOCK_MODULES = ['koji', 'rpm']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
