11197988f0d5197aeaa9afc296bed0298dc95353
Haikel Guemar <hguemar@fedoraproject.org>
2018-08-02 10:26:30 +0200

    Migrate to pbr
    - Versionning is automatically handled by pbr
    - Tox config has been updated too
    - Add coverage report

--- koji_wrapper/__init__.py
+++ koji_wrapper/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for koji_wrapper."""
+import pbr.version
 
 __author__ = """Jason Guiditta"""
 __email__ = 'jason.guiditta@gmail.com'
-__version__ = '0.1.0'
+__version__ = pbr.version.VersionInfo('koji_wrapper').version_string()
